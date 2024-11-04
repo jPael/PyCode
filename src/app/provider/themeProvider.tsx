f4964a1f2b8c206893524bc29fea3650156054d1
jPael <78292169+jPael@users.noreply.github.com>
2024-11-05 07:17:40 +0800

    themeProvider fix

--- src/app/provider/themeProvider.tsx
+++ src/app/provider/themeProvider.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: Readonly<ThemeProviderProps>) {
+	const [isLoaded, setIsLoaded] = useState(false);
+
+	useEffect(() => {
+		setIsLoaded(true);
+	}, []);
+
+	if (!isLoaded) {
+		return null;
+	}
+
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
