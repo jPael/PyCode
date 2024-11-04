c226146acf1682fe70d5a099bb623d7ee8626393
jPael <78292169+jPael@users.noreply.github.com>
2024-11-05 07:20:41 +0800

    minor fixes

--- src/components/app/codingPanel.tsx
+++ src/components/app/codingPanel.tsx
@@ -4,20 +4,20 @@ import { python } from "@codemirror/lang-python";
 import type { Extension } from "@codemirror/state";
 import CodeMirror from "@uiw/react-codemirror";
 import { useTheme } from "next-themes";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 export default function CodingPanel() {
 	const IDE = useRef(null);
 	const { theme } = useTheme();
 
-	useEffect(() => {
-		const editorElement = IDE.current?.editor.wrapperElement;
-		console.log(editorElement);
-		if (!editorElement) return;
-		const backgroundColor = window.getComputedStyle(editorElement).getPropertyValue("background-color");
+	// useEffect(() => {
+	// 	const editorElement = IDE.current?.editor.wrapperElement;
+	// 	console.log(editorElement);
+	// 	if (!editorElement) return;
+	// 	const backgroundColor = window.getComputedStyle(editorElement).getPropertyValue("background-color");
 
-		console.log("Code Mirror bg color: ", backgroundColor);
-	}, [IDE]);
+	// 	console.log("Code Mirror bg color: ", backgroundColor);
+	// }, [IDE]);
 
 	return (
 		<div className=" col-span-3 border h-full overflow-y-scroll ">
